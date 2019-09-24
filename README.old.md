0299352b6a5af5b9d56075fce8a3ebe027a69dbc
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-24 10:29:13 -0400

    [create-react-app] set up react app boilerplate

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# mirror-concept-website
+A concept website for an "always connected" mirror with a social media aspect. Created for 5D Fundamentals class as Northeastern during the Fall 2019 semester.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./common/components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
