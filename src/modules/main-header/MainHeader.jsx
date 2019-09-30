e7d8beb964d64874292621bae3a6f6f440e680ad
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-29 22:03:36 -0400

    [interaction] add button icon to scroll down site

--- src/modules/main-header/MainHeader.jsx
+++ src/modules/main-header/MainHeader.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import "./main-header.css";
 import { Col, Button, Row, Container } from "react-bootstrap";
+import autoBind from "react-autobind";
 
 export default class MainHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    autoBind(this);
+  }
+  scrollToBottom() {
+    // In the css, the height of the main header is set to 85vh, and the header is the top.
+    window.scroll({
+      left: 0,
+      top: window.innerHeight * 0.75,
+      behavior: "smooth"
+    });
+  }
+
   render() {
     return (
       <Container className="main-header mr-0 ml-0">
@@ -14,9 +29,9 @@ export default class MainHeader extends Component {
             </h2>
           </Col>
           <Col xs={12} sm={6} md={4} className="content-button-column">
-            <Button size="lg" variant="outline-dark">
-              Learn More
-            </Button>
+            <p className="m-0" onClick={this.scrollToBottom}>
+              <i className="cta-button far p-3 fa-arrow-alt-circle-down"></i>
+            </p>
           </Col>
         </Row>
       </Container>
