c5309ebf6cf4172549a690b9ead23b0536b26c7c
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-29 20:13:29 -0400

    [styling] update main header font

--- src/modules/main-header/MainHeader.jsx
+++ src/modules/main-header/MainHeader.jsx
@@ -8,7 +8,10 @@ export default class MainHeader extends Component {
       <Container className="main-header mr-0 ml-0">
         <Row className="main-header-content">
           <Col xs={12} sm={6} md={8} className="content-text-column">
-            <h2 className="content-text">JOIN THE FUTURE OF FASHION</h2>
+            <h2 className="content-text roboto-font">
+              JOIN THE FUTURE <br />
+              OF FASHION
+            </h2>
           </Col>
           <Col xs={12} sm={6} md={4} className="content-button-column">
             <Button size="lg" variant="outline-dark">
