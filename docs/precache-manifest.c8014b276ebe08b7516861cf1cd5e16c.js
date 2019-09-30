2ccf9570e22a3a5f606e2a9aa2e0c38456b8bb92
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-30 16:46:15 -0400

    [deploy] deploy product desc modules changes

---
+++ docs/precache-manifest.c8014b276ebe08b7516861cf1cd5e16c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1e1bdc0b71ec3affbed36792ce7dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51f00ec844eac35a4e45",
+    "url": "/static/css/main.cfda97c5.chunk.css"
+  },
+  {
+    "revision": "84f2a042ce2e6305964e",
+    "url": "/static/js/2.9101086f.chunk.js"
+  },
+  {
+    "revision": "51f00ec844eac35a4e45",
+    "url": "/static/js/main.00697f40.chunk.js"
+  },
+  {
+    "revision": "a62e317783646cde64eb",
+    "url": "/static/js/runtime-main.827e1455.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.451c88a9b85067dd8a48173363488f32.js"
+  "/precache-manifest.c8014b276ebe08b7516861cf1cd5e16c.js"
 );
 
 self.addEventListener('message', (event) => {
