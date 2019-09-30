e700216fcbf2246ccafa07ef4d55e6bdbe5e91eb
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-09-29 22:47:06 -0400

    [common-comp] add footer with link to portfolio

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import HeroImagesFeatures from "./modules/hero-image-features/HeroImagesFeatures
 import AlwaysOn from "./modules/always-on/AlwaysOn";
 import SustainableShopping from "./modules/sustainable-shopping/SustainableShopping";
 import WaitlistForm from "./modules/waitlist-form/WaitlistForm";
+import Footer from "./common/components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+      </header>
+      <body>
         <MainHeader />
         <HeroImagesFeatures />
         <AlwaysOn />
         <SustainableShopping />
         <WaitlistForm />
-      </header>
+      </body>
+      <Footer />
     </div>
   );
 }
---
+++ src/common/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="main-footer">
+        <p className="footer-text"> Concept website created by Nick Doherty</p>
+        <a className="footer-text" href="http://nicholasmdoherty.com">
+          nicholasmdoherty.com
+        </a>
+      </div>
+    );
+  }
+}
